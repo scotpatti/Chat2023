0b710246f0da355fd094b9cf526e64afc812d615
Dr. A <4411916+scotpatti@users.noreply.github.com>
2023-09-12 11:29:27 -0400

    Add project files.

---
+++ Chat2023/HandleClient.cs
@@ -0,0 +1,42 @@
+﻿using Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat2023
+{
+    internal class HandleClient
+    {
+        private TcpClient clientSocket;
+        private string clientName;
+
+        public void StartClient(TcpClient client, string name)
+        {
+            clientSocket = client;
+            clientName = name;
+            var thread = new Thread(DoChat);
+            thread.Start();
+        }
+
+        private void DoChat()
+        {
+            while (true)
+            {
+                try
+                {
+                    string dataFromClient = clientSocket.ReadString();
+                    Program.Broadcast(dataFromClient, clientName, true);
+                    Console.WriteLine($"{clientName} said: {dataFromClient}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Chat2023/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Net.Sockets;
+using System;
+using System.Net;
+using Extensions;
+
+namespace Chat2023;
+
+public static class Program
+{
+    public static Dictionary<string, TcpClient> ClientList
+        = new Dictionary<string, TcpClient>();
+    public static void Main()
+    {
+        var ServerSocket = new TcpListener(IPAddress.Any, 8888);
+        ServerSocket.Start();
+        Console.WriteLine("Chat server has started");
+        while (true)
+        {
+            try
+            {
+                var clientSocket = ServerSocket.AcceptTcpClient();
+                string username = clientSocket.ReadString().TrimEnd();
+                ClientList.Add(username, clientSocket);
+                Broadcast(" joined the chatroom.", username, false);
+                var client = new HandleClient();
+                client.StartClient(clientSocket, username);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Client aborted connection");
+            }
+        }
+    }
+
+    public static void Broadcast(string msg, string uname, bool flag)
+    {
+        foreach (var item in ClientList)
+        {
+            try { 
+                var m = flag ? uname + " says: " + msg : uname + msg + "\r\n";
+                item.Value.WriteString(m);
+            }
+            catch
+            {
+                Console.WriteLine($"Unable to write to {item.Key}");
+                ClientList.Remove(item.Key);
+            }
+        }
+    }
+    
+
+}
